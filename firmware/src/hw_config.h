d6b99e223544ec233c6ed1ad048b28d9b2dff315
Artem <artgrin74@gmail.com>
2025-11-12 15:38:20 +0500

    testStand

---
+++ firmware/src/hw_config.h
@@ -0,0 +1,33 @@
+#define DIR_1_PIN 0
+#define STEP_1_PIN 1
+#define EN_1_PIN 2
+
+#define DIR_2_PIN 3
+#define STEP_2_PIN 4
+#define EN_2_PIN 5
+
+#define DIR_3_PIN 8
+#define STEP_3_PIN 9
+#define EN_3_PIN 10
+
+#define DIR_4_PIN 11
+#define STEP_4_PIN 12
+#define EN_4_PIN 13
+
+#define HX711SCK_PIN 14
+#define HX711DT_PIN 15
+
+#define LED1_PIN 20
+#define LED2_PIN 22
+
+#define PULSE_PIN 21
+
+#define CURRENT_SENCE_ADC_PIN 26
+
+#define SPI0_MISO_PIN 16
+#define SPI0_MOSI_PIN 17
+#define SPI0_SCK_PIN 18
+#define SPI0_CS_PIN 19
+
+#define EMPTY_0_PIN 6
+#define EMPTY_1_PIN 7
